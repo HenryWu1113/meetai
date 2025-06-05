5464c572222416f91d2f704f5692a353b488189e
HenryWu1113 <bug910284@gmail.com>
2025-06-06 00:46:14 +0800

    05: 第三方登入驗證實作

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,109 +1,18 @@
-'use client'
+import { HomeView } from '@/modules/home/ui/views/home-view'
+import { auth } from '@/lib/auth'
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
 
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { useState } from 'react'
-import { authClient } from '@/lib/auth-client'
+const Page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
 
-export default function Home() {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-
-  const { data: session } = authClient.useSession()
-
-  const onSubmit = async () => {
-    const response = await authClient.signUp.email(
-      {
-        name,
-        email,
-        password
-      },
-      {
-        onError(context) {
-          window.alert(context.error.message)
-        },
-        onSuccess() {
-          window.alert('Sign up success')
-        }
-      }
-    )
+  if (!session) {
+    redirect('/sign-in')
   }
 
-  const onLogin = async () => {
-    const response = await authClient.signIn.email(
-      {
-        email,
-        password
-      },
-      {
-        onError(context) {
-          window.alert(context.error.message)
-        },
-        onSuccess() {
-          window.alert('Login success')
-        }
-      }
-    )
-  }
-
-  if (session) {
-    return (
-      <div className='flex flex-col gap-y-4 p-4'>
-        <p>Logged in as {session.user.name}</p>
-        <Button onClick={() => authClient.signOut()}>Sign Out</Button>
-      </div>
-    )
-  }
-
-  return (
-    <div className='flex flex-col gap-y-10'>
-      <div className='flex flex-col gap-y-4 p-4'>
-        <Input
-          type='text'
-          placeholder='name'
-          autoComplete='off'
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <Input
-          type='email'
-          placeholder='email'
-          value={email}
-          autoComplete='off'
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-          type='password'
-          placeholder='password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button onClick={onSubmit}>Create User</Button>
-      </div>
-      <div className='flex flex-col gap-y-4 p-4'>
-        <Input
-          type='text'
-          placeholder='name'
-          autoComplete='off'
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <Input
-          type='email'
-          placeholder='email'
-          value={email}
-          autoComplete='off'
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-          type='password'
-          placeholder='password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button onClick={onLogin}>Login</Button>
-      </div>
-    </div>
-  )
+  return <HomeView />
 }
+
+export default Page
