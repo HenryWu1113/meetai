16c9a1eba23870de91ca2199065c9a0b9bcba333
henry <henrywu@billionwatts.com.tw>
2025-06-06 17:37:40 +0800

    微調 className 錯誤

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -9,7 +9,7 @@ const Layout = ({ children }: Props) => {
   return (
     <SidebarProvider>
       <DashboardSidebar />
-      <main className="flex flex-col h-screen w-screen bg-mute">
+      <main className="flex flex-col h-screen w-screen bg-muted">
         {children}
       </main>
     </SidebarProvider>
