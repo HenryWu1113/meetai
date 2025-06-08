808cd70aa6ad3e2cc1d58fa3437de765ad03db29
HenryWu1113 <bug910284@gmail.com>
2025-06-08 23:24:10 +0800

    07: dashboard-navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,19 +1,21 @@
-import { SidebarProvider } from "@/components/ui/sidebar";
-import { DashboardSidebar } from "@/modules/dashboard/ui/components/dashboard-sidebar";
+import { SidebarProvider } from '@/components/ui/sidebar'
+import { DashboardSidebar } from '@/modules/dashboard/ui/components/dashboard-sidebar'
+import { DashboardNavbar } from '@/modules/dashboard/ui/components/dashboard-navbar'
 
 interface Props {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 
 const Layout = ({ children }: Props) => {
   return (
     <SidebarProvider>
       <DashboardSidebar />
-      <main className="flex flex-col h-screen w-screen bg-muted">
+      <main className='flex flex-col h-screen w-screen bg-muted'>
+        <DashboardNavbar />
         {children}
       </main>
     </SidebarProvider>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
