a609477593bf6c10cc633c8ef8a3c5c4b8011484
henry <henrywu@billionwatts.com.tw>
2025-06-10 11:36:20 +0800

    10: responsive-dialog

--- src/modules/dashboard/ui/components/dashboard-command.tsx
+++ src/modules/dashboard/ui/components/dashboard-command.tsx
@@ -1,23 +1,23 @@
 import {
-  CommandDialog,
+  CommandResponsiveDialog,
   CommandInput,
   CommandItem,
-  CommandList
-} from '@/components/ui/command'
-import { Dispatch, SetStateAction } from 'react'
+  CommandList,
+} from "@/components/ui/command";
+import { Dispatch, SetStateAction } from "react";
 
 interface Props {
-  open: boolean
-  setOpen: Dispatch<SetStateAction<boolean>>
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
 }
 
 export const DashboardCommand = ({ open, setOpen }: Props) => {
   return (
-    <CommandDialog open={open} onOpenChange={setOpen}>
-      <CommandInput placeholder='Find a metting or agent' />
+    <CommandResponsiveDialog open={open} onOpenChange={setOpen}>
+      <CommandInput placeholder="Find a metting or agent" />
       <CommandList>
         <CommandItem>test</CommandItem>
       </CommandList>
-    </CommandDialog>
-  )
-}
+    </CommandResponsiveDialog>
+  );
+};
