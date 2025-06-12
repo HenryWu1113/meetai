cb0d89188386cb16788d6e71767de2e1faf020e0
henry <henrywu@billionwatts.com.tw>
2025-06-12 16:21:20 +0800

    12: agents data table

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,26 +1,28 @@
-import { db } from '@/db'
-import { createTRPCRouter, protectedProcedure } from '@/trpc/init'
-import { agents } from '@/db/schema'
-import { TRPCError } from '@trpc/server'
-import { agentsInsertSchema } from '../schemas'
-import { z } from 'zod'
-import { eq } from 'drizzle-orm'
+import { db } from "@/db";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { agents } from "@/db/schema";
+import { agentsInsertSchema } from "../schemas";
+import { z } from "zod";
+import { eq, getTableColumns, sql } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
   getOne: protectedProcedure
     .input(z.object({ id: z.string() }))
     .query(async ({ input }) => {
       const [data] = await db
-        .select()
+        .select({
+          meetingCount: sql<number>`5`,
+          ...getTableColumns(agents),
+        })
         .from(agents)
-        .where(eq(agents.id, input.id))
+        .where(eq(agents.id, input.id));
 
-      return data
+      return data;
     }),
   getMany: protectedProcedure.query(async () => {
-    const data = await db.select().from(agents)
+    const data = await db.select().from(agents);
 
-    return data
+    return data;
   }),
   create: protectedProcedure
     // 使用 createAgentSchema 來驗證 input
@@ -32,11 +34,11 @@ export const agentsRouter = createTRPCRouter({
         // 把 input 的資料存入 agents 資料表
         .values({
           ...input,
-          userId: ctx.auth.user.id
+          userId: ctx.auth.user.id,
         })
         // 回傳存入的資料
-        .returning()
+        .returning();
 
-      return createdAgent
-    })
-})
+      return createdAgent;
+    }),
+});
--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -1,31 +1,44 @@
-'use client'
+"use client";
 
-import { LoadingState } from '@/components/loading-state'
-import { ErrorState } from '@/components/error-state'
-import { useTRPC } from '@/trpc/client'
-import { useSuspenseQuery } from '@tanstack/react-query'
+import { LoadingState } from "@/components/loading-state";
+import { ErrorState } from "@/components/error-state";
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { DataTable } from "../components/data-table";
+import { columns } from "../components/columns";
+import { EmptyState } from "@/components/empty-state";
 
 export const AgentsView = () => {
-  const trpc = useTRPC()
-  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions())
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
-  return <div>{JSON.stringify(data, null, 2)}</div>
-}
+  return (
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+      <DataTable data={data} columns={columns} />
+      {data.length === 0 && (
+        <EmptyState
+          title="Create your first agent"
+          description="Create an agent to jion your meetings. Each agent will follow your instructions and can interact with participants during the call."
+        />
+      )}
+    </div>
+  );
+};
 
 export const AgentsViewLoading = () => {
   return (
     <LoadingState
-      title='Loading Agents'
-      description='This may take a few seconds'
+      title="Loading Agents"
+      description="This may take a few seconds"
     />
-  )
-}
+  );
+};
 
 export const AgentsViewError = () => {
   return (
     <ErrorState
-      title='Error Loading Agents'
-      description='Please try again later'
+      title="Error Loading Agents"
+      description="Please try again later"
     />
-  )
-}
+  );
+};
