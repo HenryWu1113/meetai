9e4da98ca974b1121ea22d56e91d537fcd308209
henry <henrywu@billionwatts.com.tw>
2025-06-18 18:06:31 +0800

    18: meetings data table

--- src/modules/agents/types.ts
+++ src/modules/agents/types.ts
@@ -1,6 +1,8 @@
-import { inferRouterOutputs } from '@trpc/server'
+import { inferRouterOutputs } from "@trpc/server";
 
-import type { AppRouter } from '@/trpc/routers/_app'
+import type { AppRouter } from "@/trpc/routers/_app";
 
 // 從 _app 的 appRouter 定義 agents 的 getOne 的輸出型別
-export type AgentGetOne = inferRouterOutputs<AppRouter>['agents']['getOne']
+export type AgentGetOne = inferRouterOutputs<AppRouter>["agents"]["getOne"];
+export type AgentGetMany =
+  inferRouterOutputs<AppRouter>["agents"]["getMany"]["items"];
