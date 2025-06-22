50142ff85e49af83e27bc817a563fe00fc0e10d4
HenryWu1113 <bug910284@gmail.com>
2025-06-22 16:11:38 +0800

    24: background jobs

--- src/modules/meetings/types.ts
+++ src/modules/meetings/types.ts
@@ -1,6 +1,7 @@
 import { inferRouterOutputs } from '@trpc/server'
 
 import type { AppRouter } from '@/trpc/routers/_app'
+import { number, string } from 'zod'
 
 // 從 _app 的 appRouter 定義 meetings 的 getOne 的輸出型別
 export type MeetingGetOne = inferRouterOutputs<AppRouter>['meetings']['getOne']
@@ -12,4 +13,11 @@ export enum MeetingStatus {
   Completed = 'completed',
   Processing = 'processing',
   Cancelled = 'cancelled'
+}
+export type StreamTranscriptItem = {
+  speaker_id: string
+  type: string
+  text: string
+  start_ts: number
+  stop_ts: number
 }
